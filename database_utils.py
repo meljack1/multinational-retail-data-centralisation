7737713bd9492af74c770e1623ff560985204b59
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-02 13:46:24 +0000

    Adds .gitignore file and creates method in DatabaseConnector class to read .yaml file to get db credentials

--- database_utils.py
+++ database_utils.py
@@ -1,1 +1,7 @@
+import yaml
+
 class DatabaseConnector:
+    def read_db_creds(self):
+        with open('db_creds.yaml', 'r') as file:
+            db_creds = yaml.safe_load(file)
+            return(db_creds)
