e9419a37f53652ee3bf7ad910a3cfbb22c0e22e5
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-02 14:40:50 +0000

    Adds method to initialise sqlalchemy engine

--- database_utils.py
+++ database_utils.py
@@ -1,7 +1,16 @@
 import yaml
+from sqlalchemy import create_engine
+
 
 class DatabaseConnector:
     def read_db_creds(self):
         with open('db_creds.yaml', 'r') as file:
             db_creds = yaml.safe_load(file)
             return(db_creds)
+    def init_db_engine(self):
+        db_creds = self.read_db_creds()
+        engine = create_engine(f"postgresql+psycopg2://{db_creds['RDS_USER']}:{db_creds['RDS_PASSWORD']}@{db_creds['RDS_HOST']}:{db_creds['RDS_PORT']}/{db_creds['RDS_DATABASE']}")
+        return engine
+
+dbc = DatabaseConnector()
+print(dbc.init_db_engine())
