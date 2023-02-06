9482dbbb4839bcafc94ccb60a680cf6b4c43613a
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-06 19:23:30 +0000

    Adds method to read a table by name to DataExtractor class

--- data_extraction.py
+++ data_extraction.py
@@ -1,2 +1,13 @@
+import database_utils as du
+import pandas as pd
+
 class DataExtractor:
-    
+    def read_rds_table(self, dc, table):
+        """ Connects to database and returns a table by name as a pandas dataframe
+        """
+        engine = dc.init_db_engine().connect()
+        return pd.read_sql_table(table, engine)
+
+de = DataExtractor()
+dc = du.DatabaseConnector()
+dc.list_db_tables()
