6ccbb38825be26ca311c1ed774a66bd338ff3aee
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-13 20:30:21 +0000

    In data cleaning function - removes null values, cleans country codes

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,1 +1,25 @@
+import data_extraction as de
+import pandas as pd
+
 class DataCleaning:
+    def __init__(self):
+        self.dex = de.DataExtractor()
+    def clean_user_data(self):
+        """ Reads user data from table legacy_users
+        """
+        user_data = self.dex.read_rds_table("legacy_users")
+        """ Removes rows containing NULL uuid
+        """
+        inconsistent_rows = user_data["user_uuid"].isin(["NULL"])
+        user_data = user_data[~inconsistent_rows]
+        """ Cleans country_code to only contain actual country codes 
+        with 2 capital letters. Transforms country_code from object to 
+        category.
+        """
+        country_codes = user_data['country_code'].str.fullmatch("([A-Z][A-Z])")
+        user_data = user_data[country_codes]
+        user_data['country_code'] = user_data['country_code'].astype('category')
+        return user_data
+
+dc = DataCleaning()
+print(dc.clean_user_data())
--- data_extraction.py
+++ data_extraction.py
@@ -2,12 +2,15 @@ import database_utils as du
 import pandas as pd
 
 class DataExtractor:
-    def read_rds_table(self, dc, table):
+    def __init__(self):
+        self.dc = du.DatabaseConnector()
+    def read_rds_table(self, table):
         """ Connects to database and returns a table by name as a pandas dataframe
         """
-        engine = dc.init_db_engine().connect()
+        engine = self.dc.init_db_engine().connect()
         return pd.read_sql_table(table, engine)
 
 de = DataExtractor()
+
 dc = du.DatabaseConnector()
 dc.list_db_tables()
