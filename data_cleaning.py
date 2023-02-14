95d27afcb545d743fc423ef227cceb89a83cabed
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-14 18:44:30 +0000

    Adds date format automation

--- data_cleaning.py
+++ data_cleaning.py
@@ -8,10 +8,14 @@ class DataCleaning:
         """ Reads user data from table legacy_users
         """
         user_data = self.dex.read_rds_table("legacy_users")
+        print("Read user data")
+
         """ Removes rows containing NULL uuid
         """
         inconsistent_rows = user_data["user_uuid"].isin(["NULL"])
         user_data = user_data[~inconsistent_rows]
+        print("Cleaned inconsistencies")
+
         """ Cleans country_code to only contain actual country codes 
         with 2 capital letters. Transforms country_code from object to 
         category.
@@ -19,7 +23,25 @@ class DataCleaning:
         country_codes = user_data['country_code'].str.fullmatch("([A-Z][A-Z])")
         user_data = user_data[country_codes]
         user_data['country_code'] = user_data['country_code'].astype('category')
-        return user_data
+        print("Cleaned country codes")
+
+        """ Validates emails with regex
+        """
+        #email_validated = user_data['email_address'].str.fullmatch("^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")
+        #user_data = user_data[email_validated]
+        #print("Cleaned email data")
+
+        """ Corrects and validates phone numbers
+        """
+        ##
+        
+        """ Corrects and validates dates
+        """
+        user_data["join_date"] = pd.to_datetime(user_data["join_date"], infer_datetime_format=True, errors='coerce') 
+        user_data["date_of_birth"] = pd.to_datetime(user_data["date_of_birth"], infer_datetime_format=True, errors='coerce') 
+        print("Cleaned dates")
+
+        return user_data["join_date"][1349]
 
 dc = DataCleaning()
 print(dc.clean_user_data())
