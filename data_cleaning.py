90c374186c675b5bf4f33ce9613c2efb54e4dc61
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-14 21:13:21 +0000

    First draft of phone number standardisation added

--- data_cleaning.py
+++ data_cleaning.py
@@ -1,20 +1,18 @@
 import data_extraction as de
 import pandas as pd
+import re
 
 class DataCleaning:
     def __init__(self):
         self.dex = de.DataExtractor()
+    def standardise_phone_number(self, phone_number):
+        phone_str = str(re.sub("([-\s])|(\+..)|(\(.+\))|^(0[0-9]+?\s?)", "", str(phone_number))).lstrip("0")
+        return "00" + phone_str
     def clean_user_data(self):
         """ Reads user data from table legacy_users
         """
         user_data = self.dex.read_rds_table("legacy_users")
         print("Read user data")
-
-        """ Removes rows containing NULL uuid
-        """
-        inconsistent_rows = user_data["user_uuid"].isin(["NULL"])
-        user_data = user_data[~inconsistent_rows]
-        print("Cleaned inconsistencies")
 
         """ Cleans country_code to only contain actual country codes 
         with 2 capital letters. Transforms country_code from object to 
@@ -27,21 +25,33 @@ class DataCleaning:
 
         """ Validates emails with regex
         """
-        #email_validated = user_data['email_address'].str.fullmatch("^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$")
-        #user_data = user_data[email_validated]
-        #print("Cleaned email data")
+        email_validated = user_data['email_address'].str.fullmatch(".+\@.+\..+")
+        user_data = user_data[email_validated]
+        print("Cleaned email data")
 
         """ Corrects and validates phone numbers
         """
-        ##
-        
+        for index, row in user_data.iterrows():
+            row = row.copy()
+            user_data["phone_number"][index] = self.standardise_phone_number(row["phone_number"])
+        #user_data.loc[user_data['phone_number'].str.len() != 10] = pd.NA
+        #null_phone_numbers = user_data["phone_number"].isnull()
+        #print("Number of null phone numbers:", null_phone_numbers.sum())
+        #user_data = user_data.dropna(subset=["phone_number"])
+
         """ Corrects and validates dates
         """
         user_data["join_date"] = pd.to_datetime(user_data["join_date"], infer_datetime_format=True, errors='coerce') 
         user_data["date_of_birth"] = pd.to_datetime(user_data["date_of_birth"], infer_datetime_format=True, errors='coerce') 
         print("Cleaned dates")
 
-        return user_data["join_date"][1349]
+        """ Removes rows containing NULL uuid
+        """
+        inconsistent_rows = user_data["user_uuid"].isin(["NULL"])
+        user_data = user_data[~inconsistent_rows]
+        print("Cleaned inconsistencies")
+
+        return user_data
 
 dc = DataCleaning()
 print(dc.clean_user_data())
