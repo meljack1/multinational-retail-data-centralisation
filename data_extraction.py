7563383a3cc16cfa21c1d92ebe7009ad7c0b0d5b
Mel Jack <melissa_jack@hotmail.co.uk>
2023-02-22 20:08:49 +0000

    Adds method to list number of stores to data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,6 +1,9 @@
 #import database_utils as du
 import pandas as pd
 import tabula
+import requests
+
+header = {"x-api-key": "yFBQbwXe9J3sd6zWVAMrK6lcxxr0q1lr2PT6DDMX"}
 
 class DataExtractor:
     def read_rds_table(self, table, du):
@@ -12,8 +15,13 @@ class DataExtractor:
         tabula.convert_into(link, "data.csv", output_format="csv", pages='all')
         df = pd.read_csv('data.csv')
         return df
+    def list_number_of_stores(self, endpoint, header):
+        number_of_stores = requests.get(endpoint, headers=header)
+        return number_of_stores
 
 de = DataExtractor()
+
+print(de.list_number_of_stores("https://aqj7u5id95.execute-api.eu-west-1.amazonaws.com/prod/number_stores", header))
 
 #print(de.retrieve_pdf_data("https://data-handling-public.s3.eu-west-1.amazonaws.com/card_details.pdf"))
 
